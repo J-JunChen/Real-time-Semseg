e9068527f7077035c46fbdb339ba7695e01a3559
Jun-jieChen <cjj@hdu.edu.cn>
2021-03-04 16:16:07 +0800

    The first complete run.

---
+++ utils/util.py
@@ -0,0 +1,54 @@
+import os 
+import numpy as np
+from PIL import Image
+
+import torch
+from torch import nn
+
+
+class AverageMeter(object):
+    """ Computers and stores the average and current value """
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+    
+def poly_learning_rate(base_lr, curr_iter, max_iter, power=0.9):
+    """ poly learning rate policy """
+    lr = base_lr * (1 - float(curr_iter) / max_iter) ** power
+    return lr
+
+def intersectionAndUnionGPU(output, target, K, ignore_index=255):
+    # 'K' classes, output and target sizes are N or N * L or N * H * W, each value in range 0 to k-1
+    assert (output.dim() in [1, 2, 3])
+    assert output.shape == target.shape
+    output = output.view(-1)
+    target = target.view(-1)
+    output[target == ignore_index] = ignore_index
+    intersection = output[output == target]
+    area_intersection = torch.histc(intersection, bins=K, min=0, max=K-1)
+    area_output = torch.histc(output, bins=K, min=0, max=K-1)
+    area_target = torch.histc(output, bins=K, min=0, max=K-1)
+    area_union = area_output + area_target - area_intersection
+    return area_intersection, area_union, area_target
+    
+
+def find_free_port():
+    import socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Binding to port 0 will cause the OS to find an available port for us
+    sock.bind(("", 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    # NOTE: there is still a chance the port could be taken by other processes.
+    return port
